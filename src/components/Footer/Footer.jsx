c315232566157e585cda2b1d6c4dc5fdb22aa747
loka mabari <lokamabari@gmail.com>
2023-03-11 23:14:13 +0000

    footer belum responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png"
+import '../../styles/footer.css'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+  return <footer className="footer">
+    <Container>
+      <Row>
+        <Col lg='3' md='4' sm='6'>
+          <div className="footer__logo text-start">
+            <img src={logo} alt="logo" />
+            <h5 className='footer__title'>Tasty Treat</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea dolor voluptas quasi commodi vero.
+            </p>
+          </div>
+        </Col>
+
+        <Col lg='3' md='4' sm='6'>
+          <h5>Delivery Time</h5>
+          <ListGroup className='delivery__time-list'>
+            <ListGroupItem className='delivery__time-item border-0 ps-0'>
+              <span>Sunday - Thursday</span>
+              <p>10:00am - 11:00pm</p>
+            </ListGroupItem>
+
+            <ListGroupItem className='delivery__time-item border-0 ps-0'>
+              <span>Friday - Saturday</span>
+              <p>Off Day</p>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+
+        <Col lg='3' md='4' sm='6'>
+          <h5>Contact</h5>
+          <ListGroupItem className='delivery__time-item border-0 ps-0'>
+            <span>Location: <p>Lorem ipsum dolor sit amet.</p></span>
+
+          </ListGroupItem>
+
+          <ListGroup className='delivery__time-list'>
+            <ListGroupItem className='delivery__time-item border-0 ps-0'>
+              <span>Phone: (+62) 812 3456 7891</span>
+
+            </ListGroupItem>
+
+            <ListGroupItem className='delivery__time-item border-0 ps-0'>
+              <span>Email: example@gmail.com</span>
+
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+
+        <Col lg='3' md='4' sm='6'>
+          <h5>Newsletter</h5>
+          <p>Subscribe our newsletter</p>
+          <div className="newsletter">
+            <input type="email" placeholder='Enter your email' />
+            <span><i className="ri-send-plane-line"></i></span>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className='mt-5'>
+        <Col lg='6' md='6'>
+          <p className='copyright__text'>&copy; Copyright - 2023 | Made by Polleck. All Right Reserved.</p>
+        </Col>
+        <Col lg='6' md='6'>
+          <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+            <p className='m-0'>Follow: </p>
+            <span> <Link to=''><i className="ri-facebook-line"></i></Link></span>
+            <span> <Link to=''><i className="ri-github-line"></i></Link></span>
+            <span> <Link to=''><i className="ri-youtube-line"></i></Link></span>
+            <span> <Link to=''><i className="ri-linkedin-line"></i></Link></span>
+          </div>
+        </Col>
+      </Row>
+
+    </Container>
+  </footer>
 }
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png"
@@ -27,6 +27,9 @@ const nav__link = [
 
 
 const Header = () => {
+
+  const menuRef = useRef(null)
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
   return <header className="header">
     <Container>
       <div className="nav__wrapper d-flex align-items-center justify-content-between">
@@ -35,12 +38,13 @@ const Header = () => {
           <h5>Tasty Treat</h5>
         </div>
         {/*========= Menu ========== */}
-        <div className="navigation">
+        <div className="navigation" ref={menuRef} onClick={toggleMenu}>
           <div className="menu d-flex align-items-center gap-5">
             {
               nav__link.map((item, index) => (
-                <NavLink to={item.path} key={index}
-                className={navClass => navClass.isActive ? 'active__menu' :""}
+                <NavLink
+                  to={item.path} key={index}
+                  className={navClass => navClass.isActive ? 'active__menu' : ""}
                 >{item.display}</NavLink>
               ))
             }
@@ -49,16 +53,16 @@ const Header = () => {
         {/*======== Nav Right Icon */}
         <div className="nav__right d-flex align-items-center gap-4">
           <span className="cart__icon">
-            <i class="ri-shopping-cart-2-line"></i>
-            <span className="cart_badge">2</span>
+            <i className="ri-shopping-cart-2-line"></i>
+            <span className="cart__badge">2</span>
           </span>
 
           <span className="user">
-            <Link to="/login"><i class="ri-user-line"></i></Link>
+            <Link to="/login"><i className="ri-user-line"></i></Link>
           </span>
 
-          <span className="mobile__menu">
-            <i class="ri-menu-line"></i>
+          <span className="mobile__menu" onClick={toggleMenu}>
+            <i className="ri-menu-line"></i>
           </span>
         </div>
 
