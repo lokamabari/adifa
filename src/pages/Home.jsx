2ba72c88c8aa1f22ba84ee1fe843fc374ef9dc6e
loka mabari <lokamabari@gmail.com>
2023-03-17 11:22:17 +0000

    hero content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
 import { Container, Row, Col } from 'reactstrap';
+
 import Helmet from '../components/Helmet/Helmet.js'
+
+import '../styles/hero-section.css'
 
 import heroImg from '../assets/images/hero.png'
 
@@ -12,13 +14,15 @@ const Home = () => {
         <Row>
           <Col lg='6' md='6'>
             <div className="hero__content">
-              <h5>Easy way to make an order</h5>
-              <h1><span>Hungry?</span> Just wait<br />food at  <span>your door</span></h1>
+              <h5 className=' mb-3'>Easy way to make an order</h5>
+              <h1 className=' mb-4 hero__title' ><span>HUNGRY?</span> Just wait<br />food at  <span>your door</span></h1>
             </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ullam quidem nobis distinctio, libero vitae minima a quisquam veritatis?</p>
           </Col>
 
           <Col lg='6' md='6'>
-            <div className="heo">
+            <div className="hero__img">
               <img src={heroImg} alt="hero-img" className='w-100' />
             </div>
           </Col>
