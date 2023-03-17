278bc360fce725a4c1bc2e2efecbff20080b32b9
loka mabari <lokamabari@gmail.com>
2023-03-17 22:21:45 +0000

    popular food add

--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import productImg from '../../../assets/images/product_02.1.jpg'
+import '../../../styles/product-card.css'
 
-const ProductCard = () => {
-  return (
-    <div className='product__item'>
-        <div className="product__img">
-            <img src={productImg} alt="product-img" />
-        </div>
+const ProductCard = (props) => {
 
-        <div className="product__content">
-            <h5><Link >Burger</Link></h5>
-            <div>
-                <span className='product__price'>$24.00</span>
-                <button className='addTOCart__btn'>Add to cart</button>
+
+    const {id, title, image01, price} = props.item
+    return (
+        <div className='product__item'>
+            <div className="product__img">
+                <img src={image01} alt="product-img" className=' w-50' />
+            </div>
+
+            <div className="product__content">
+                <h5><Link to={`/foods/${id}`} >{title}</Link></h5>
+                <div className=' d-flex align-items-center justify-content-between'>
+                    <span className='product__price'>${price}</span>
+                    <button className='addTOCart__btn'>Add to cart</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,35 +4,73 @@ import { Link } from 'react-router-dom';
 
 import Helmet from '../components/Helmet/Helmet.js'
 import Category from '../components/UI/category/Category.jsx';
+import ProductCard from '../components/UI/product-card/ProductCard.jsx';
 
 import '../styles/hero-section.css'
 import '../styles/home.css'
+
+import products from '../assets/fake-data/products.js'
 
 import heroImg from '../assets/images/hero.png'
 import featureImg01 from '../assets/images/service-01.png'
 import featureImg02 from '../assets/images/service-02.png'
 import featureImg03 from '../assets/images/service-03.png'
 
+import foodCategoryImg01 from '../assets/images/hamburger.png'
+import foodCategoryImg02 from '../assets/images/pizza.png'
+import foodCategoryImg03 from '../assets/images/bread.png'
+
+import { useState, useEffect } from 'react';
+
+const fetaureData = [
+  {
+    title: 'Quick Delivery',
+    imgUrl: featureImg01,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, accusantium?'
+  },
+  {
+    title: 'Super Dine in',
+    imgUrl: featureImg02,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, accusantium?'
+  },
+  {
+    title: 'Easy Pick Up',
+    imgUrl: featureImg03,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, accusantium?'
+  },
+]
+
 const Home = () => {
 
+  const [category, setCategory] = useState('ALL')
+  const [allProducts, setAllproducts] = useState(products)
 
-  const fetaureData = [
-    {
-      title:'Quick Delivery',
-      imgUrl: featureImg01,
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, accusantium?'
-    },
-    {
-      title:'Super Dine in',
-      imgUrl: featureImg02,
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, accusantium?'
-    },
-    {
-      title:'Easy Pick Up',
-      imgUrl: featureImg03,
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, accusantium?'
-    },
-  ]
+  useEffect(() => {
+
+    if (category === 'ALL') {
+      setAllproducts(products)
+    }
+
+    if (category === 'BURGER') {
+      const fillteredProducts = products.filter(item => item.category === 'Burger')
+
+      setAllproducts(fillteredProducts)
+    }
+
+    if (category === 'PIZZA') {
+      const fillteredProducts = products.filter(item => item.category === 'Pizza')
+
+      setAllproducts(fillteredProducts)
+    }
+
+    if (category === 'BREAD') {
+      const fillteredProducts = products.filter(item => item.category === 'Bread')
+
+      setAllproducts(fillteredProducts)
+    }
+
+  },[category])
+
   return <Helmet title='Home'>
     <section>
       <Container>
@@ -42,18 +80,18 @@ const Home = () => {
               <h5 className=' mb-3'>Easy way to make an order</h5>
               <h1 className=' mb-4 hero__title' ><span>HUNGRY?</span> Just wait<br />food at  <span>your door</span></h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ullam quidem nobis distinctio, libero vitae minima a quisquam veritatis?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ullam quidem nobis distinctio, libero vitae minima a quisquam veritatis?</p>
 
-            <div className="hero__btns d-flex align-items-center gap-5 mt-4">
-              <button className='order__btn d-flex align-items-center justify-content-between'>Order now <i className="ri-arrow-right-s-line"></i></button>
+              <div className="hero__btns d-flex align-items-center gap-5 mt-4">
+                <button className='order__btn d-flex align-items-center justify-content-between'>Order now <i className="ri-arrow-right-s-line"></i></button>
 
-              <button className='all__foods-btn' ><Link to='/foods' >See all foods</Link></button>
-            </div>
-            <div className='hero__service d-flex align-items-center gap-5 mt-5'>
-              <p className=' d-flex align-items-center gap-2'><span className='shipping__icon'><i className="ri-car-line"></i></span> No shipping charge</p>
+                <button className='all__foods-btn' ><Link to='/foods' >See all foods</Link></button>
+              </div>
+              <div className='hero__service d-flex align-items-center gap-5 mt-5'>
+                <p className=' d-flex align-items-center gap-2'><span className='shipping__icon'><i className="ri-car-line"></i></span> No shipping charge</p>
 
-              <p className='d-flex align-items-center gap-2'><span className='shipping__icon'><i className="ri-shield-check-line"></i></span> 100% secure checkoout</p>
-            </div>
+                <p className='d-flex align-items-center gap-2'><span className='shipping__icon'><i className="ri-shield-check-line"></i></span> 100% secure checkoout</p>
+              </div>
             </div>
           </Col>
 
@@ -82,7 +120,7 @@ const Home = () => {
             <p className=' feature__text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, accusamus.</p>
           </Col>
           {
-            fetaureData.map((item,index)=>(
+            fetaureData.map((item, index) => (
               <Col lg='4' md='4' key={index} className=' mt-5' >
                 <div className="feature__item text-center px-5 py-3" >
                   <img src={item.imgUrl} alt="feature-img" className=' w-25 mb-3' />
@@ -92,6 +130,40 @@ const Home = () => {
               </Col>
             ))
           }
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12' className=' text-center' >
+            <h5>Popular Foods</h5>
+          </Col>
+
+          <Col lg='12'>
+            <div className="food__category d-flex align-items-center justify-content-center gap-4">
+              <button className="all__btn " onClick={() => setCategory('ALL')} >All</button>
+              <button className=' d-flex align-items-center gap-2' onClick={() => setCategory('BURGER')}>
+                <img src={foodCategoryImg01} alt="" />Burger
+              </button>
+              <button className='d-flex align-items-center gap-2' onClick={() => setCategory('PIZZA')}>
+                <img src={foodCategoryImg02} alt="" />Pizza
+              </button>
+              <button className='d-flex align-items-center gap-2' onClick={() => setCategory('BREAD')}>
+                <img src={foodCategoryImg03} alt="" />Bread
+              </button>
+            </div>
+          </Col>
+
+          {
+            products.map(item => (
+              <Col lg='3' md='4' key={item.id} className='mt-5'>
+                <ProductCard item={item} />
+              </Col>
+            ))
+          }
+
         </Row>
       </Container>
     </section>
