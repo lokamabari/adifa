ec725c37fed86be70ede6c572a7426622cd46933
loka mabari <lokamabari@gmail.com>
2023-03-17 11:09:33 +0000

    home

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Helmet = (props) => {
-    document.title = 'Food Ordering App -' + props.title;
+    document.title = 'Food Ordering App - ' + props.title;
     return <div className='w-100'>{props.children}</div>;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import { Container, Row, Col } from 'reactstrap';
+import Helmet from '../components/Helmet/Helmet.js'
+
+import heroImg from '../assets/images/hero.png'
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <Helmet title='Home'>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' md='6'>
+            <div className="hero__content">
+              <h5>Easy way to make an order</h5>
+              <h1><span>Hungry?</span> Just wait<br />food at  <span>your door</span></h1>
+            </div>
+          </Col>
+
+          <Col lg='6' md='6'>
+            <div className="heo">
+              <img src={heroImg} alt="hero-img" className='w-100' />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Helmet>
 }
 
 export default Home;
